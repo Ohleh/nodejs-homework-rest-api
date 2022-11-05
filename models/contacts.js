571d50abde7a9839415b71c7a6b36836504cabbf
Ohleh <oleg.kolischuk@gmail.com>
2022-11-05 23:02:37 +0200

    add Schema, model

--- models/contacts.js
+++ models/contacts.js
@@ -1,73 +1,99 @@
-const fs = require("fs/promises");
-const { nanoid } = require("nanoid");
-const path = require("path");
+const { Schema, model } = require("mongoose");
 
-const contactsPath = path.join(__dirname, "contacts.json");
-const update = async (contacts) =>
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+// створили схему
+const contactsSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-const listContacts = async () => {
-  const response = await fs.readFile(contactsPath, "utf-8");
-  const contacts = JSON.parse(response);
-  return contacts;
-};
+// створюємо модель (викликаємо функцію модел)
+const Contact = model("contact", contactsSchema);
 
-const getContactById = async (contactId) => {
-  const response = await listContacts();
-  const contactsById = response.find((el) => el.id === contactId);
-  return contactsById || null;
-};
+module.exports = Contact;
 
-const removeContact = async (contactId) => {
-  const response = await listContacts();
-  const findId = response.find((el) => el.id === contactId);
-  if (!findId) return null;
+// ///////////////
 
-  const removedList = response.filter((el) => el.id !== contactId);
-  // const [result] = books.splice(index, 1);
-  const newContactList = [...removedList];
+// const fs = require("fs/promises");
+// const { nanoid } = require("nanoid");
+// const path = require("path");
 
-  await update(newContactList);
-  return newContactList;
-};
+// const contactsPath = path.join(__dirname, "contacts.json");
+// const update = async (contacts) =>
+//   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-const addContact = async (body) => {
-  const response = await listContacts();
-  const { name, email, phone } = body;
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-  response.push(newContact);
+// const listContacts = async () => {
+//   const response = await fs.readFile(contactsPath, "utf-8");
+//   const contacts = JSON.parse(response);
+//   return contacts;
+// };
 
-  await update(response);
-  return newContact;
-};
+// const getContactById = async (contactId) => {
+//   const response = await listContacts();
+//   const contactsById = response.find((el) => el.id === contactId);
+//   return contactsById || null;
+// };
 
-const updateContact = async (contactId, body) => {
-  const response = await listContacts();
-  const { name, email, phone } = body;
-  const index = response.findIndex((el) => el.id === contactId);
-  if (index === -1) {
-    return null;
-  }
-  response[index] = {
-    id: contactId,
-    name,
-    email,
-    phone,
-  };
+// const removeContact = async (contactId) => {
+//   const response = await listContacts();
+//   const findId = response.find((el) => el.id === contactId);
+//   if (!findId) return null;
 
-  await update(response);
-  return response[index];
-};
+//   const removedList = response.filter((el) => el.id !== contactId);
+//   const newContactList = [...removedList];
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+//   await update(newContactList);
+//   return newContactList;
+// };
+
+// const addContact = async (body) => {
+//   const response = await listContacts();
+//   const { name, email, phone } = body;
+//   const newContact = {
+//     id: nanoid(),
+//     name,
+//     email,
+//     phone,
+//   };
+//   response.push(newContact);
+
+//   await update(response);
+//   return newContact;
+// };
+
+// const updateContact = async (contactId, body) => {
+//   const response = await listContacts();
+//   const { name, email, phone } = body;
+//   const index = response.findIndex((el) => el.id === contactId);
+//   if (index === -1) {
+//     return null;
+//   }
+//   response[index] = {
+//     id: contactId,
+//     name,
+//     email,
+//     phone,
+//   };
+
+//   await update(response);
+//   return response[index];
+// };
+
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// };
