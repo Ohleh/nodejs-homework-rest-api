d3d40b1f3e78203b24b23bad90e4f78730537606
Ohleh <oleg.kolischuk@gmail.com>
2022-11-24 19:30:54 +0200

    wh fully done, accept addition

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -3,7 +3,7 @@ const { Users } = require("../../models/users");
 const logout = async (req, res) => {
   const { _id } = req.user;
   await Users.findByIdAndUpdate(_id, { token: "" });
-  res.json({
+  res.status(204).json({
     message: "Logout success",
   });
 };
