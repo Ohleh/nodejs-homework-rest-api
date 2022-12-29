b1963a45a33a83f178679dd36f43887b8458fc52
Ohleh <oleg.kolischuk@gmail.com>
2022-12-29 16:26:09 +0200

    add sendemail to helpers

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const ctrlWrapper = require("./ctrlWrapper");
 const RequestError = require("./RequestError");
 const handleSaveErrors = require("./handleSaveErrors");
+const makeSend = require("./sendemail");
 
 module.exports = {
   ctrlWrapper,
   RequestError,
   handleSaveErrors,
+  makeSend,
 };
